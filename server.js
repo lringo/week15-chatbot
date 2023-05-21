83284ef961bcc8f8b743eb14fcef0183ab6fe397
Liina Rin <lringo@tktk.ee>
2023-05-21 15:56:31 +0300

    add chatbot

---
+++ server.js
@@ -0,0 +1,108 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const http = require('http');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/search', (req, res) => {
+    res.render('search', { movieDetails:'' });
+});
+
+app.post('/search', (req, res) => {
+    let userMovieTitle = req.body.movieTitle;
+
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=fed0579523be006c8483df4c1246f7a6&query=${userMovieTitle}`;
+    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=fed0579523be006c8483df4c1246f7a6&language=en-US';
+    
+    let endpoints = [movieUrl, genresUrl];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then(axios.spread((movie, genres) => {
+        const [movieRaw] = movie.data.results;
+        let movieGenreIds =movieRaw.genre_ids;
+        let movieGenres = genres.data.genres;
+        
+        let movieGenresArray = [];
+
+        for(let i = 0; i < movieGenreIds.length; i++){
+            for(let j = 0; j < movieGenres.length; j++) {
+                if(movieGenreIds[i] === movieGenres[j].id){
+                    movieGenresArray.push(movieGenres[j].name);
+                }
+            }
+        }
+        
+        let genresToDisplay = '';
+        movieGenresArray.forEach(genre => {
+            genresToDisplay = genresToDisplay + `${genre}, `;
+        });
+
+        genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+
+        
+
+    let movieData = {
+        title: movieRaw.title,
+        year: new Date(movieRaw.release_date).getFullYear(),
+        genres: genresToDisplay,
+        overview: movieRaw.overview,
+        posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
+    };
+
+    res.render('search', {movieDetails: movieData});
+
+    }));
+
+});
+
+app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
+});
+
+app.listen(process.env.PORT || 3000, () => {
+});
